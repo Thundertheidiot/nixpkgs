dc3d0befa8e69091003aac4761e491c6143249e6
Thundertheidiot <58880811+Thundertheidiot@users.noreply.github.com>
2024-10-13 19:12:42 +0300

    Fix fetchSavannah
    GNU Savannah either broke something or changed their snapshot format.

--- pkgs/build-support/fetchsavannah/default.nix
+++ pkgs/build-support/fetchsavannah/default.nix
@@ -6,7 +6,7 @@ lib.makeOverridable (
 , ... # For hash agility
 }@args: fetchzip ({
   inherit name;
-  url = "https://git.savannah.gnu.org/cgit/${repo}.git/snapshot/${repo}-${rev}.tar.gz";
+  url = "https://git.savannah.gnu.org/gitweb/?p=${repo}.git;a=snapshot;h=${rev};sf=tgz";
   meta.homepage = "https://git.savannah.gnu.org/cgit/${repo}.git/";
 } // removeAttrs args [ "repo" "rev" ]) // { inherit rev; }
 )
